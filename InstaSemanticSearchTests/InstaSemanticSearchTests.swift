ba31d01fb1652304ca76066b6b8d98608a669eb9
LuigiR5 <147706705+LuigiR5@users.noreply.github.com>
2026-03-02 20:37:34 +0000

    Initial commit

---
+++ InstaSemanticSearchTests/InstaSemanticSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaSemanticSearchTests.swift
+//  InstaSemanticSearchTests
+//
+//  Created by Rork on March 2, 2026.
+//
+
+import Testing
+@testable import InstaSemanticSearch
+
+struct InstaSemanticSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# instagram-semantic-ios
-Created by Rork
